1b2feb6d47c04fbf3fa20427ac8078494e2db647
Annatar <itsannatar@gamil.com>
2024-08-01 18:59:24 +0330

    feat: add background for home page within Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,50 @@
-import { CircularProgress, Container, useTheme } from "@mui/material";
+import type { Theme } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Container,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
+import PATH from "../app/pages/routes/path";
+import background from "../assets/background.png";
+import backgrounMobile from "../assets/background-mobile.png";
 import Bottombar from "./navigation/Bottombar";
 import Navbar from "./navigation/Navbar";
 
 const Layout = () => {
   const theme = useTheme();
+  // use media query to differ layout appearance on desktop and mobile screen sizes
+  const matches = useMediaQuery((t: Theme) => t.breakpoints.up("md"));
+  const isHomePage = useLocation().pathname === PATH.HOME;
+
   return (
-    <>
+    <Box
+      sx={
+        isHomePage
+          ? matches
+            ? {
+                width: "100dvw",
+                height: "100dvh",
+                backgroundImage: `url(${background})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }
+            : {
+                width: "100dvw",
+                height: "100dvh",
+                backgroundImage: `url(${backgrounMobile})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center ",
+                backgroundSize: "cover",
+              }
+          : null
+      }
+    >
       <Navbar />
       <Container>
         <Suspense
@@ -27,7 +63,7 @@ const Layout = () => {
         </Suspense>
       </Container>
       <Bottombar />
-    </>
+    </Box>
   );
 };
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     background: {
-      default: "#222835",
+      default: "#101010",
     },
     text: {
       primary: "#ffff",
