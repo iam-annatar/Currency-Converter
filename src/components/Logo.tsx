3d1b0b25fd3c947c37824011105b08155f3f4889
Annatar <itsannatar@gamil.com>
2024-08-01 03:53:59 +0330

    refactor: link main logo to home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
+import PATH from "../app/pages/routes/path";
 import MainLogo from "../assets/icons/currency-icon.svg?react";
 
 const Logo = () => {
   return (
-    <Box
-      sx={{
+    <Link
+      style={{
+        textDecoration: "none",
         display: "flex",
         gap: ".5rem",
         flexDirection: "column",
         alignItems: "center",
       }}
+      to={PATH.HOME}
     >
       <MainLogo />
       <Typography fontSize="1.5rem" fontWeight="bold" color="#EFBF3A">
         AM Swap
       </Typography>
-    </Box>
+    </Link>
   );
 };
 
