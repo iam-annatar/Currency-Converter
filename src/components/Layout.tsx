f36980066d2e4f5dc9b987bac2f53b964694d18c
Annatar <itsannatar@gamil.com>
2024-08-01 16:18:35 +0330

    feat: add loader

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { CircularProgress, Container, useTheme } from "@mui/material";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
@@ -6,11 +6,23 @@ import Bottombar from "./navigation/Bottombar";
 import Navbar from "./navigation/Navbar";
 
 const Layout = () => {
+  const theme = useTheme();
   return (
     <>
       <Navbar />
       <Container>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense
+          fallback={
+            <CircularProgress
+              style={{
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                color: theme.palette.secondary.main,
+              }}
+            />
+          }
+        >
           <Outlet />
         </Suspense>
       </Container>
