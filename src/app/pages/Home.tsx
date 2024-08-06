d046550f8afa03728232b7d05500fe2874de3ad5
Annatar <itsannatar@gamil.com>
2024-08-06 18:08:25 +0330

    feat: add a home page content with animation

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,5 +1,60 @@
+import type { Theme } from "@mui/material";
+import { Button, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+import CustomAnimation from "../../components/Custom-animation";
+import PATH from "./routes/path";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const theme = useTheme();
+  const matches = useMediaQuery((t: Theme) => t.breakpoints.down("sm"));
+  return (
+    <main>
+      <header
+        style={
+          matches
+            ? { marginTop: "8rem", textAlign: "center" }
+            : { marginTop: "8rem" }
+        }
+      >
+        <CustomAnimation>
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
+          >
+            <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+              Exchange Rates <br /> at Your Fingertips
+            </Typography>
+            <Typography fontSize="1.3rem" color="darkgray">
+              Get the latest and most accurate exchange rates for over <br />{" "}
+              100 currencies and easly convert between them.
+            </Typography>{" "}
+            <NavLink to={PATH.SWAP}>
+              <Button
+                sx={{
+                  maxWidth: "fit-content",
+                  padding: ".5rem 1rem",
+                  borderRadius: "2rem",
+                  textTransform: "none",
+                  fontSize: "1.125rem",
+                  fontWeight: "bold",
+                  bgcolor: theme.palette.secondary.main,
+                  ":active": {
+                    bgcolor: theme.palette.secondary.dark,
+                  },
+                  ":hover": {
+                    bgcolor: theme.palette.secondary.dark,
+                  },
+                }}
+                variant="contained"
+              >
+                Start Swapping
+              </Button>
+            </NavLink>
+          </div>
+        </CustomAnimation>
+      </header>
+    </main>
+  );
 };
 
 export default Home;
