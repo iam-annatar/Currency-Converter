1268cd5f2a7ba6e5ad1f596a8a4bdc76932a1e2e
Annatar <itsannatar@gamil.com>
2024-08-06 15:59:07 +0330

    feat: add sofia font to the project

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,9 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: "SofiaPro-Light, sans-serif",
+  },
   palette: {
     background: {
       default: "#101010",
