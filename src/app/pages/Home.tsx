7a7d0ee4aa3622b77e122ddc73e43c66e5631f3c
Annatar <itsannatar@gamil.com>
2024-08-08 18:27:31 +0330

    fix: button in home page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -34,29 +34,30 @@ const Home = () => {
               Get the latest and most accurate exchange rates for over <br />{" "}
               100 currencies and easly convert between them.
             </Typography>{" "}
-            <NavLink to={PATH.SWAP}>
-              <Button
-                sx={{
-                  maxWidth: "fit-content",
-                  padding: ".5rem 1rem",
-                  borderRadius: "2rem",
-                  textTransform: "none",
-                  fontSize: "1.125rem",
-                  fontWeight: "bold",
-                  bgcolor: theme.palette.secondary.main,
-                  ":active": {
-                    bgcolor: theme.palette.secondary.dark,
-                  },
-                  ":hover": {
-                    bgcolor: theme.palette.secondary.dark,
-                  },
-                }}
-                variant="contained"
-              >
-                Start Swapping
-              </Button>
-            </NavLink>
           </div>
+          <NavLink to={PATH.SWAP}>
+            <Button
+              sx={{
+                maxWidth: "fit-content",
+                padding: ".7rem 1rem",
+                borderRadius: "2rem",
+                textTransform: "none",
+                fontSize: "1.125rem",
+                fontWeight: "bold",
+                mt: "2rem",
+                bgcolor: theme.palette.secondary.main,
+                ":active": {
+                  bgcolor: theme.palette.secondary.dark,
+                },
+                ":hover": {
+                  bgcolor: theme.palette.secondary.dark,
+                },
+              }}
+              variant="contained"
+            >
+              Start Swapping
+            </Button>
+          </NavLink>
         </CustomAnimation>
       </header>
     </main>
